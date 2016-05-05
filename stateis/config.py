95bb5383f51f7557c9ae101718a067eb77c5375d
Adam <adam@dhcp-10-142-233-180.uawifi.arizona.edu>
2016-05-05 13:40:11 -0700

    Added config file for stateis

---
+++ stateis/config.py
@@ -0,0 +1,13 @@
+"""
+Configuration file for stateis.py consisting of username, password, and list of IP addresses
+
+Example:
+
+user = "admin"
+password = "securepwd123"
+hosts = ["111.111.111.111", "222.222.222.222"]
+"""
+
+user = ""
+password = ""
+hosts = [""]
