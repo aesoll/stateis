65b4b397df2a628174db4dc338dc8a38ca72619d
Adam Soll <solladame@gmail.com>
2016-09-15 13:53:07 -0700

    Updated quotes in config for consistency

--- stateis/config.py
+++ stateis/config.py
@@ -1,13 +1,13 @@
-"""
+'''
 Configuration file for stateis.py consisting of username, password, and list of IP addresses
 
 Example:
 
-user = "admin"
-password = "securepwd123"
-hosts = ["111.111.111.111", "222.222.222.222"]
-"""
+user = 'admin'
+password = 'securepwd123'
+hosts = ['111.111.111.111', '222.222.222.222']
+'''
 
-user = ""
-password = ""
-hosts = [""]
+user = ''
+password = ''
+hosts = ['']
